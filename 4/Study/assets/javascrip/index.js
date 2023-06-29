6b3c1ee0d6e372529c0f5075a48bdc82927dc5d5
Khoi TranVan <khoi.tranvan>
2023-06-29 20:48:12 +0700

    bt

---
+++ 4/Study/assets/javascrip/index.js
@@ -0,0 +1,32 @@
+// let yourName = prompt('Hãy nhập tên của bạn');
+// document.write('Xin chào ' + yourName);
+
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+
+document.write('<br/>')
+let width = 20
+let height = 10
+let area = width * height
+document.write('Area = ' + area)
+
+
+let c = prompt('c')
+d = prompt('d')
+if (c % d == 0) {
+  alert('c chia het cho d')
+}
+else {
+  alert('c khong chia het cho d')
+}
